1699f2098a7cb4318c0b6efb747dc827a2e99217
Major Tuvok <kevin.iselborn@t-online.de>
2018-01-04 17:15:53 +0100

    Build mistake...

--- src/main/java/mt/mcmods/spellcraft/common/spell/components/conditions/AbsSpellCondition.java
+++ src/main/java/mt/mcmods/spellcraft/common/spell/components/conditions/AbsSpellCondition.java
@@ -2,6 +2,6 @@ package mt.mcmods.spellcraft.common.spell.components.conditions;
 
 import mt.mcmods.spellcraft.common.spell.components.AbsSpellComponent;
 
-public abstract class AbsSpellCondition extends AbsSpellComponent<ISpellCondition> {
+public abstract class AbsSpellCondition extends AbsSpellComponent<ISpellCondition> implements ISpellCondition {
 
 }
--- src/main/java/mt/mcmods/spellcraft/common/spell/components/executables/AbsSpellExecutable.java
+++ src/main/java/mt/mcmods/spellcraft/common/spell/components/executables/AbsSpellExecutable.java
@@ -2,6 +2,6 @@ package mt.mcmods.spellcraft.common.spell.components.executables;
 
 import mt.mcmods.spellcraft.common.spell.components.AbsSpellComponent;
 
-public abstract class AbsSpellExecutable extends AbsSpellComponent<ISpellExecutable> {
+public abstract class AbsSpellExecutable extends AbsSpellComponent<ISpellExecutable> implements ISpellExecutable {
 
 }
